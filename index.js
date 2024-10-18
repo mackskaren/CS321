c18365717fdec5491a1055558a99881ad3cc9677
Max Karen <mackskaren@yahoo.com>
2024-10-18 14:46:39 -0700

    updated environment

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
-import dotenv from 'dotenv'
-dotenv.config()
+// Watch my Discord Bot Project Tutorial video here: https://youtu.be/pDQAn18-2go - Discord Bot Tutorial | JavaScript & Node.js
 
-import { Client } from 'discord.js';
+require('dotenv').config();
 
-const client = new Client({
-    intents: [
-        
-    ]
+const axios = require('axios');
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+
+client.on('ready', () => {
+    console.log('bot is ready');
 })
+
+client.on('messageCreate', async (message) => {
+    if (message.content === 'ping') {
+        message.reply({
+            content: 'pong',
+        })
+    }
+    else if (message.content === 'quote') {
+        let resp = await axios.get(`https://api.quotable.io/random`);
+        const quote = resp.data.content;
+
+        message.reply({
+            content: quote,
+        })
+    }
+})
+
+client.login(process.env.DISCORD_BOT_ID);
---
+++ index_other.js
@@ -0,0 +1,10 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+import { Client } from 'discord.js';
+
+const client = new Client({
+    intents: [
+        
+    ]
+})
