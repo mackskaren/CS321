165ba5f1d663aa2a3aff4139fc58fa746fb86169
Max Karen <mackskaren@yahoo.com>
2024-10-27 20:46:06 -0400

    adding slash command functionality and splitting up the project a bit

--- index.js
+++ index.js
@@ -1,30 +1,94 @@
 // Watch my Discord Bot Project Tutorial video here: https://youtu.be/pDQAn18-2go - Discord Bot Tutorial | JavaScript & Node.js
 
-require('dotenv').config();
+// require('dotenv').config();
 
+const {token} = require('./config.json');
 const axios = require('axios');
-const { Client, GatewayIntentBits } = require('discord.js');
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, Events, GatewayIntentBits, ButtonBuilder, ButtonStyle, ModalBuilder } = require('discord.js');
+
+const client = new Client({ 
+    intents: [GatewayIntentBits.Guilds, 
+        GatewayIntentBits.GuildMessages, 
+        GatewayIntentBits.MessageContent] 
+    });
 
 
 client.on('ready', () => {
     console.log('bot is ready');
 })
 
-client.on('messageCreate', async (message) => {
-    if (message.content === 'ping') {
-        message.reply({
-            content: 'pong',
-        })
-    }
-    else if (message.content === 'quote') {
-        let resp = await axios.get(`https://api.quotable.io/random`);
-        const quote = resp.data.content;
+const button = new ButtonBuilder()
+    .setCustomId('testbutton')
+    .setLabel("Button 1")
+    .setStyle(ButtonStyle.Primary);
 
-        message.reply({
-            content: quote,
-        })
-    }
-})
+client.commands = new Collection();
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
 
-client.login(process.env.DISCORD_BOT_ID);
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		// Set a new item in the Collection with the key as the command name and the value as the exported module
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
+}
+
+client.on(Events.InteractionCreate, interaction => {
+	console.log(interaction);
+});
+
+client.on(Events.InteractionCreate, interaction => {
+	if (!interaction.isChatInputCommand()) return;
+	console.log(interaction);
+});
+
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+		} else {
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
+	}
+});
+
+// client.on('messageCreate', async (message) => {
+//     if (message.content === 'ping') {
+//         message.reply({
+//             content: 'pong',
+//         })
+//     }
+//     else if (message.content === 'quote') {
+//         let resp = await axios.get(`https://api.quotable.io/random`);
+//         const quote = resp.data.content;
+
+//         message.reply({
+//             content: quote,
+//         })
+//     }
+// })
+
+// client.login(process.env.DISCORD_BOT_ID);
+client.login(token);
